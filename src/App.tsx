93eacee0d4996c4f9ed7fc729c28f6f5afa2d712
maniprasadkadlaskar <maniprasadvkadlaskar@gmail.com>
2025-06-14 11:27:45 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <>
+      <h1
+        className="p-5 text-3xl font-bold text-center mt-10 mb-5 bg-purple-400"
+      >
+        Select AI
+      </h1>
+    </>
+  )
+}
+
+export default App
